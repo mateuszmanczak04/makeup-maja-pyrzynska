0779356c6cfcc3b0e0709de479eefdca9ffa24de
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2022-12-17 10:33:47 +0100

    change base url to heroku

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ const nextConfig = {
   env: {
     // BASE_URL: 'http://localhost:3000',
     // BASE_URL: 'https://makeup-maja-pyrzynska-mateuszmanczak04.vercel.app/',
-    BASE_URL: 'https://main.d1d4b0xenh480y.amplifyapp.com',
+    // BASE_URL: 'https://main.d1d4b0xenh480y.amplifyapp.com',
+    BASE_URL: 'https://makeup-maja-pyrzynska.herokuapp.com',
   },
 
   // future: {
